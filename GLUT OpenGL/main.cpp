e80800f30f66b0d316af94237b1508f94ac0481c
tuanminh <tuan-minh.le@cpe.fr>
2025-03-25 14:48:13 +0100

    Initial Commit

---
+++ GLUT OpenGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GLUT OpenGL
+//
+//  Created by tuan minh on 25/3/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
